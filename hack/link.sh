22469658afddc06633e80801078b306a1ff53dfd
Rafal Proszowski <paroxp@gmail.com>
2019-08-23 21:29:28 +0100

    Automatically linkup useful files

---
+++ hack/link.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -eu
+
+OS="${OS:-linux}"
+
+function link {
+  if [ "${2:-false}" == "true" ]; then
+    mkdir -p "~/${1}"
+  fi
+
+  ln -sf "~/.dot/${1}" "~/${1}"
+}
+
+if [ "${OS}" == "linux" ]; then
+  link .config/compton true
+  link .config/dunst true
+  link .config/gtk-3.0 true
+  link .config/i3 true
+  link .config/polybar true
+  link .config/rofi true
+  link .config/termite true
+  link .config/xfce4 true
+
+  link .gtkrc-2.0
+  link .gtkrc-2.0.mine
+  link .Xresources
+fi
+
+link .gnupg true
+link .gemrc
+link .gitconfig
+link .vimrc
+link .zshrc
+
+# mkdir -p ~/.oh-my-zsh/themes/
+# link .oh-my-zsh/themes/rafal.zsh-theme
