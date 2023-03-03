d318d41f62adf31f349fada016370d7438f76a7d
Rafal Proszowski <paroxp@gmail.com>
2023-03-03 17:36:27 +0000

    gpg: enable module properly

--- gpg.nix
+++ gpg.nix
@@ -1,26 +1,30 @@
 { ... }:
 
 let cfg = import ./config.nix; in {
-  programs.gpg.settings = {
-    auto-key-locate = "keyserver";
-    keyserver = "hkps://keyserver.ubuntu.com";
-    keyserver-options = "no-honor-keyserver-url";
-    personal-cipher-preferences = "AES256 AES192 AES CAST5";
-    personal-digest-preferences = "SHA512 SHA384 SHA256 SHA224";
-    default-preference-list = "SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES CAST5 ZLIB BZIP2 ZIP Uncompressed";
-    cert-digest-algo = "SHA512";
-    s2k-cipher-algo = "AES256";
-    s2k-digest-algo = "SHA512";
-    charset = "utf-8";
-    fixed-list-mode = true;
-    no-comments = true;
-    no-emit-version = true;
-    keyid-format = "0xlong";
-    list-options = "show-uid-validity";
-    verify-options = "show-uid-validity";
-    with-fingerprint = true;
-    use-agent = true;
-    require-cross-certification = true;
+  programs.gpg = {
+    enable = true;
+
+    settings = {
+      auto-key-locate = "keyserver";
+      keyserver = "hkps://keyserver.ubuntu.com";
+      keyserver-options = "no-honor-keyserver-url";
+      personal-cipher-preferences = "AES256 AES192 AES CAST5";
+      personal-digest-preferences = "SHA512 SHA384 SHA256 SHA224";
+      default-preference-list = "SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES CAST5 ZLIB BZIP2 ZIP Uncompressed";
+      cert-digest-algo = "SHA512";
+      s2k-cipher-algo = "AES256";
+      s2k-digest-algo = "SHA512";
+      charset = "utf-8";
+      fixed-list-mode = true;
+      no-comments = true;
+      no-emit-version = true;
+      keyid-format = "0xlong";
+      list-options = "show-uid-validity";
+      verify-options = "show-uid-validity";
+      with-fingerprint = true;
+      use-agent = true;
+      require-cross-certification = true;
+    };
   };
 
   services.gpg-agent = {
@@ -28,6 +32,7 @@ let cfg = import ./config.nix; in {
 
     defaultCacheTtl = 60;
     enableSshSupport = true;
+    enableExtraSocket = true;
     maxCacheTtl = 120;
     pinentryFlavor = cfg.pinentry;
   };
