71d214cbe5fab0978823413aaa47902835e73c69
Rafal Proszowski <paroxp@gmail.com>
2023-03-15 14:35:21 +0000

    devenv: bump to 0.6.2

--- devenv.nix
+++ devenv.nix
@@ -3,7 +3,7 @@
 {
   home.packages = with pkgs; [
     cachix # Really shouldn't be keeping this in home-manager...
-    (import (fetchTarball https://github.com/cachix/devenv/archive/v0.6.tar.gz)).default
+    (import (fetchTarball https://github.com/cachix/devenv/archive/v0.6.2.tar.gz)).default
   ];
 
   programs.direnv = {
