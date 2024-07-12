62ac751766243e933e1c248d88e2ec8dc21791c0
Rafal Proszowski <paroxp@gmail.com>
2024-07-12 14:54:06 +0100

    zsh: update zsh-nix-shell

--- zsh.nix
+++ zsh.nix
@@ -47,7 +47,7 @@ in {
         src = fetchFromGitHub {
           owner = "chisui";
           repo = "zsh-nix-shell";
-          rev = "v0.5.0";
+          rev = "v0.8.0";
           sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
         };
       }
