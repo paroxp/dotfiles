18873c8f98999dd8b612de9e2f5663dfce191da5
Rafal Proszowski <paroxp@gmail.com>
2024-07-12 14:55:07 +0100

    devenv: stop using custom import

--- devenv.nix
+++ devenv.nix
@@ -2,8 +2,7 @@
 
 {
   home.packages = with pkgs; [
-    cachix # Really shouldn't be keeping this in home-manager...
-    (import (fetchTarball https://github.com/cachix/devenv/archive/v0.6.2.tar.gz)).default
+    pkgs.devenv
   ];
 
   programs.direnv = {
