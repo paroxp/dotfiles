4338c2f4b0706be66b8509d211c1217646aa4dd6
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2025-03-24 17:56:36 +0000

    vscode: address deprecation warning

--- vscode.nix
+++ vscode.nix
@@ -4,27 +4,29 @@
   programs.vscode = {
     enable = true;
 
-    userSettings = {
-      "editor.rulers" = [80 120];
-      "files.autoSave" = "onFocusChange";
-      "workbench.colorTheme" = "Default Dark+";
-      "cSpell.language" = "en-GB";
-      "cSpell.enableFiletypes" = [
-        "ruby"
-      ];
-      "window.menuBarVisibility" = "toggle";
-    };
+    profiles.default = {
+      userSettings = {
+        "editor.rulers" = [80 120];
+        "files.autoSave" = "onFocusChange";
+        "workbench.colorTheme" = "Default Dark+";
+        "cSpell.language" = "en-GB";
+        "cSpell.enableFiletypes" = [
+          "ruby"
+        ];
+        "window.menuBarVisibility" = "toggle";
+      };
 
-    extensions = with pkgs.vscode-extensions; [
-      bbenoist.nix
-      dbaeumer.vscode-eslint
-      denoland.vscode-deno
-      eamodio.gitlens
-      editorconfig.editorconfig
-      golang.go
-      hashicorp.terraform
-      rust-lang.rust-analyzer
-      streetsidesoftware.code-spell-checker
-    ];
+      extensions = with pkgs.vscode-extensions; [
+        bbenoist.nix
+        dbaeumer.vscode-eslint
+        denoland.vscode-deno
+        eamodio.gitlens
+        editorconfig.editorconfig
+        golang.go
+        hashicorp.terraform
+        rust-lang.rust-analyzer
+        streetsidesoftware.code-spell-checker
+      ];
+    };
   };
 }
