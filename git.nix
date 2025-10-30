3acb222ea57fab5fb2506d84e2a1f4beb999caae
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2025-10-30 10:38:38 +0000

    git: update schema

--- git.nix
+++ git.nix
@@ -3,13 +3,15 @@
 let cfg = import ./config.nix; in {
   programs.git = with pkgs; {
     enable = true;
-    package = gitAndTools.gitFull;
-    userEmail = cfg.user.email;
-    userName = cfg.user.fullName;
     signing.key = cfg.user.gpgID;
     signing.signByDefault = true;
 
-    extraConfig = {
+    settings = {
+      user = {
+        email = cfg.user.email;
+        name = cfg.user.fullName;
+      };
+
       color = { ui = "auto"; };
       core = { whitespace = "trailing-space,space-before-tab"; editor = "vim"; };
       github = { user = cfg.user.github; };
@@ -18,19 +20,19 @@ let cfg = import ./config.nix; in {
       push = { default = "tracking"; };
       rebase = { autoSquash = "true"; };
       rerere = { enabled = "false"; };
-    };
 
-    aliases = {
-      branch = "branch -ra";
-      ca = "commit --amend";
-      caa = "commit -a --amend -C HEAD";
-      co = "checkout";
-      diff = "diff --word-diff";
-      grep = "grep -Ii";
-      ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]\" --decorate --numstat";
-      ls = "log --pretty=format:\"%C(green)%h\\ %C(yellow)[%ad]%Cred%d\\ %Creset%s%Cblue\\ [%cn]\" --decorate --date=relative";
-      pushf = "push --force-with-lease";
-      status = "status -s";
+      alias = {
+        branch = "branch -ra";
+        ca = "commit --amend";
+        caa = "commit -a --amend -C HEAD";
+        co = "checkout";
+        diff = "diff --word-diff";
+        grep = "grep -Ii";
+        ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]\" --decorate --numstat";
+        ls = "log --pretty=format:\"%C(green)%h\\ %C(yellow)[%ad]%Cred%d\\ %Creset%s%Cblue\\ [%cn]\" --decorate --date=relative";
+        pushf = "push --force-with-lease";
+        status = "status -s";
+      };
     };
 
     ignores = [
